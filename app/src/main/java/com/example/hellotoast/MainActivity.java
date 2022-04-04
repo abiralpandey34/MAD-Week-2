d8f98e6fb242bbf4944ca440c261061e8400828a
Abiral <abiral.pandey34@gmail.com>
2022-04-04 19:23:19 +0545

    Homework Complete

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -1,23 +1,54 @@
 package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private int mCount = 0;
-    private TextView mShowCount;
+    private int currentCount = 0;
+    TextView numberText;
+    Button toastButton, countButton, zeroButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mShowCount = (TextView) findViewById(R.id.textView);
-        //findViewById(R.id.textView);
+
+        countButton = findViewById(R.id.count_btn);
+        toastButton = findViewById(R.id.toast_btn);
+        zeroButton = findViewById(R.id.zero_btn);
+        numberText = findViewById(R.id.count_text);
+        this.changeCountColor();
+
+        countButton.setOnClickListener(this);
+        toastButton.setOnClickListener(this);
+        zeroButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.count_btn:
+                countUp(view);
+                break;
+
+            case R.id.toast_btn:
+                showToast(view);
+                break;
+
+            case R.id.zero_btn:
+                this.currentCount = 0;
+                this.zeroButton.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.gray));
+                numberText.setText(this.currentCount+"");
+                this.changeCountColor();
+                break;
+        }
     }
 
     public void showToast(View view) {
@@ -25,8 +56,20 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
-    public void countUp(View view) {
-        mCount++;
-        mShowCount.setText(Integer.toString(mCount));
+    public void changeCountColor(){
+        // set background color of count button
+        if(this.currentCount%2==0){
+            this.countButton.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.teal_200));
+            return;
+        }
+
+        this.countButton.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.red));
+    }
+
+    public void countUp(View view){
+        currentCount++;
+        numberText.setText(currentCount+"");
+        this.zeroButton.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.pink));
+        this.changeCountColor();
     }
 }
